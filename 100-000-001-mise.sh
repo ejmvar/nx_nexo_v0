2aaf7952f390c395665063ad1dc82a85e28376c9
Eduardo Martinez <ejmvar@gmail.com>
2026-01-16 17:31:54 -0300

    update .mise.toml configuration and add mise.sh script for Nx project setup

---
+++ 100-000-001-mise.sh
@@ -0,0 +1,58 @@
+
+
+mise install
+mise doctor
+
+mise run 100-000-001-nx-pnpm-install
+
+# ------------------------------
+# # If necessary to approve builds:
+# pnpm approve-builds -g
+# # or
+# pnpm approve-builds nx -g
+# ------------------------------
+
+mise run 100-000-002-nx-CREATE-pnpm-project-here
+# custom, React, Next.js, 
+
+# ------------------------------
+# [100-000-002-nx-CREATE-pnpm-project-here] $ npx create-nx-workspace@latest nexo-prj --package-manager=pnpm
+
+#  NX   Let's create a new workspace [https://nx.dev/getting-started/intro]
+
+# ✔ Which starter do you want to use? · custom
+# ✔ Which stack do you want to use? · react
+# ✔ What framework would you like to use? · next
+# ✔ Application name · nexo-prj
+# ✔ Would you like to use the App Router (recommended)? · Yes
+# ✔ Would you like to use the src/ directory? · Yes
+# ✔ Which unit test runner would you like to use? · jest
+# ✔ Test runner to use for end to end (E2E) tests · playwright
+# ✔ Default stylesheet format · tailwind
+# ✔ Would you like to use ESLint? · Yes
+# ✔ Would you like to use Prettier for code formatting? · Yes
+# ✔ Try the full Nx platform? · yes
+
+#  NX   Creating your v22.3.3 workspace.
+
+# ✔ Installing dependencies with pnpm
+# ✔ Successfully created the workspace: nexo-prj
+# ✔ CI workflow has been generated successfully
+
+#  NX   Directory is already under version control. Skipping initialization of git.
+
+
+#  NX   Push your workspace to GitHub.
+
+# Go to https://github.com/new?name=nexo-prj and push this workspace.
+
+# ✔ Nx Cloud has been set up successfully
+
+#  NX   Your platform setup is almost complete.
+
+# Push your repo, then go to Nx Cloud and finish the setup: https://cloud.nx.app/connect/VZA7Is66wH
+# qq@qq-omen:/W/NEXO/nx_nexo_v0.info/NEXO/nx_nexo_v0$ 
+
+# ------------------------------
+
+
