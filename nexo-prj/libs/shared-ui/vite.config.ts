b19d2d48d61c9aed82eb8cea4712f8e82d1276da
Eduardo Martinez <ejmvar@gmail.com>
2026-01-22 19:41:11 -0300

    feat(shared-ui): remove deprecated files and update configuration for Vitest

--- nexo-prj/libs/shared-ui/vite.config.ts
+++ nexo-prj/libs/shared-ui/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -27,5 +28,11 @@ export default defineConfig({
     },
     minify: false,
     sourcemap: false,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test-setup.ts'],
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
